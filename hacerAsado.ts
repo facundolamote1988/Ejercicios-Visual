d4c50d58d40ebfe8efa4a9b9b866ddb107c3f1c9
facundolamote1988 <faculamote@gmail.com>
2023-03-18 20:36:33 -0300

    cambios

--- hacerAsado.ts
+++
@@ -1,6 +0,0 @@
-console.log("hacer un asado");
-paso1 -prender el fuego-
-paso2 -condimentar la carne-
-paso3 -poner la carne en la parrila-
-paso4 -echarle braza debajo de la parrilla-
-paso5  -cocinar 30min de cada lado y retirar-
--- hacerAsado/aSado.ts
+++
@@ -1,6 +0,0 @@
-console.log("hacer un asado");
-console.log("paso 1"); 
-console.log("paso 2");
-console.log("paso 3"); 
-console.log("paso 4"); 
-console.log("paso 5");
