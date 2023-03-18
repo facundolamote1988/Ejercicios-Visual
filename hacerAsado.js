45a6f84714fa5410f513a738fdfe86a3c3890495
facundolamote1988 <faculamote@gmail.com>
2023-03-18 19:28:04 -0300

    asado

---
+++ hacerAsado.js
@@ -0,0 +1,27 @@
+console.log("hacer un asado");
+paso1 - prender;
+el;
+fuego -
+    paso2 - condimentar;
+la;
+carne -
+    paso3 - poner;
+la;
+carne;
+en;
+la;
+parrila -
+    paso4 - echarle;
+braza;
+debajo;
+de;
+la;
+parrilla -
+    paso5 - cocinar;
+30;
+min;
+de;
+cada;
+lado;
+y;
+retirar - ;
